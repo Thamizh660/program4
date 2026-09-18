67ff947a3f31c91666eca5814a378c093b452876
THAMIZH ILAKIYA <thamizhelakiya.29@gmail.com>
2026-09-18 13:07:13 +0530

    Update student_solution.sh

--- student_solution.sh
+++ student_solution.sh
@@ -5,29 +5,33 @@
 # Register Number:
 
 echo "===== SELinux Status ====="
-
+sestatus
+getenforce
 
 echo "===== Creating Web Directory ====="
-
+mkdir -p /var/www/html/testdir
 echo "===== Creating HTML File ====="
-
+ /var/www/html/testdir/index.html
 
 echo "===== Setting Linux Permissions ====="
-
+chmod -R 755 /var/www/html/testdir
 
 echo "===== Checking Initial Context ====="
-
+ls -Z /var/www/html/testdir/index.html
 
 echo "===== Assigning Wrong SELinux Context ====="
-
+chcon -t user_home_t /var/www/html/testdir/index.html
 echo "===== Checking Wrong Context ====="
-
+ls -Z /var/www/html/testdir/index.html
 
 echo "===== Checking AVC Denials ====="
-echo "===== Correcting SELinux Context ====="
+curl http://localhost/testdir/index.html
+grep "denied" /var/log/audit/audit.log | tail -n 5
 
+echo "===== Correcting SELinux Context ====="
+restorecon -v /var/www/html/testdir/index.html
 
 echo "===== Checking Correct Context ====="
-
+ls -Z /var/www/html/testdir/index.html
 
 echo "===== Practical Completed ====="
